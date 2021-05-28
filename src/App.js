b883cec9644cc80943d6bc859f13a71d06b037e4
sagarcb <sagar.cb.009@gmail.com>
2021-05-28 12:49:14 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: 'Dr. Mahfuz',
+    job: "Singer"
+  };
+
+  var styles = {
+    color: 'red',
+    backgroundColor: 'yellow'
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit done <code>src/App.js</code> and save to reload.
         </p>
+        <h1 style={styles}>My heading: {person.name + " Job: "+person.job}</h1>
+        <h1 style={{color: 'white', backgroundColor: 'black'}}>My heading: {person.name + " Job: "+person.job}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
